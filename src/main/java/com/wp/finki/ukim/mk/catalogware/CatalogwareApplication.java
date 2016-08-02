01af7724eb68321fba99b7579765cf1f3d40d641
k1ko <mkd.k1ko@gmail.com>
2016-08-03 01:27:22 +0200

    project init

--- src/main/java/com/wp/finki/ukim/mk/catalogware/CatalogwareApplication.java
+++ src/main/java/com/wp/finki/ukim/mk/catalogware/CatalogwareApplication.java
@@ -2,11 +2,21 @@ package com.wp.finki.ukim.mk.catalogware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class CatalogwareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogwareApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
 	}
 }
--- src/test/java/com/wp/finki/ukim/mk/catalogware/CatalogwareApplicationTests.java
+++ src/test/java/com/wp/finki/ukim/mk/catalogware/CatalogwareApplicationTests.java
@@ -1,16 +1,16 @@
 package com.wp.finki.ukim.mk.catalogware;
 
-import org.junit.Test;
+/*import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest*/
 public class CatalogwareApplicationTests {
 
-	@Test
+	/*@Test
 	public void contextLoads() {
 	}
-
+*/
 }
