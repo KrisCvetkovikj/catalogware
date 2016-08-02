a2492f11b5bcdca77daab18697817a15f51e1852
k1ko <mkd.k1ko@gmail.com>
2016-08-03 00:16:59 +0200

    project init

---
+++ src/main/java/com/wp/finki/ukim/mk/catalogware/CatalogwareApplication.java
@@ -0,0 +1,12 @@
+package com.wp.finki.ukim.mk.catalogware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogwareApplication.class, args);
+	}
+}
