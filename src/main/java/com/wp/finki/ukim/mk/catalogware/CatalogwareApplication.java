50bf055f46c74f187b2a5a8e5dc8f555118bb335
Borce Petrovski <bor4epetrovski@gmail.com>
2016-08-03 09:49:55 +0200

    create project structure

--- src/main/java/com/wp/finki/ukim/mk/catalogware/CatalogwareApplication.java
+++ src/main/java/com/wp/finki/ukim/mk/catalogware/CatalogwareApplication.java
@@ -1,13 +1,18 @@
 package com.wp.finki.ukim.mk.catalogware;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.slf4j.LoggerFactory.*;
 
 @Controller
 @SpringBootApplication
+@EnableWebMvc
 public class CatalogwareApplication {
 
 	public static void main(String[] args) {
